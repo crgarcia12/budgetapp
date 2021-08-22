7b5a08ce4da24222f473f12d781e724369cc3da9
AutoExtractor <crgarcia@live.com.ar>
2021-08-22 15:01:30 +0200

    new razor

---
+++ dotnetsrc/budgetapp/budgetapp/Models/Expense.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace budgetapp.Models
+{
+    public class Expense
+    {
+        public string User { get; set; }
+        public string Description { get; set; }
+        public string Shop { get; set; }
+        public bool Shared { get; set; }
+        public bool Home { get; set; }
+        public double Amount { get; set; }
+        public string PaymentMethod { get; set; }
+    }
+}
